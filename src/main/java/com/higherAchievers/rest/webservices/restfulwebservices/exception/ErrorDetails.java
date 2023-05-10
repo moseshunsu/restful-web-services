3391d9d79c28a8fd2dafd00b8ebaa9adb8528a20
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-10 15:44:56 +0100

    Created a customized exception or error message when retrieving a user with a wrong ID

---
+++ src/main/java/com/higherAchievers/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package com.higherAchievers.rest.webservices.restfulwebservices.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+}
