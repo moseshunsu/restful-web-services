03a9e3e5dc22678f559328674ade3d5e99e100e6
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-11 15:58:23 +0100

    Implemented the following     .   Spring Boot Actuator     .   Spring Boot HAL Explorer     .   H2 Database     .   Created user entity and some test data

--- src/main/java/com/higherAchievers/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/higherAchievers/rest/webservices/restfulwebservices/user/User.java
@@ -1,6 +1,9 @@
 package com.higherAchievers.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -9,8 +12,11 @@ import java.time.LocalDate;
 
 @AllArgsConstructor
 @Data
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private int id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'Moses');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Ridoh');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Pelumi');
